cd95262dc6b62045b640b928436450164b532c0a
josemarimaya <josemarimaya@gmail.com>
2024-07-20 19:26:37 +0200

    studying django

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,12 @@ from django.urls import path, include
 from . import views # Importamos las views directamente desde la app
 
 urlpatterns = [
-    path('', views.hello),
-    path('about', views.about)
+    path('', views.index),
+    path('about/', views.about),
+    path('hello/<str:username>', views.hello),
+    path('hello/<int:id>', views.hello_id),
+    path('projects/', views.projects),
+    path('tasks/<int:id>', views.tasks),
+    path('tasks/<str:title>', views.task_title)
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
-def hello(request):
-    return HttpResponse("<h2>Hello World</h2>")
+def index(request):
+    return HttpResponse("<h2>Index page</h2>")
+
+
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h2>Hello %s</h2>" %username)
+
+def hello_id(request, id):
+    print(type(id))
+    result = id + 100 * 2
+    return HttpResponse("<h2>Hello %i</h2>" %result)
 
 def about(request):
     return HttpResponse("<h1> About </h1>")
+
+def projects(request):
+    projects = list(Project.objects.values())
+    #return HttpResponse("Projects:")
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, id):
+    #task = Task.objects.get(id=id)
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse("Task: %s" % task.title)
+
+def task_title(request, title):
+    task = get_object_or_404(Task, title=title)
+    return HttpResponse("Task: %s" % task.title)
